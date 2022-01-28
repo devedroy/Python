68284a8e07304a2e94e77aaa39f53180376504c1
Roy Devpreyo <devedroy.dr@gmail.com>
2022-01-28 22:27:26 +0530

    merge edited

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,10 +1,3 @@
-# Python program for implementation of MergeSort
-
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
-
 def merge(arr, l, m, r):
 	n1 = m - l + 1
 	n2 = r - m
@@ -34,38 +27,25 @@ def merge(arr, l, m, r):
 			j += 1
 		k += 1
 
-	# Copy the remaining elements of L[], if there
-	# are any
 	while i < n1:
 		arr[k] = L[i]
 		i += 1
 		k += 1
 
-	# Copy the remaining elements of R[], if there
-	# are any
 	while j < n2:
 		arr[k] = R[j]
 		j += 1
 		k += 1
 
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
-
-
 def mergeSort(arr, l, r):
 	if l < r:
 
-		# Same as (l+r)//2, but avoids overflow for
-		# large l and h
 		m = l+(r-l)//2
 
-		# Sort first and second halves
 		mergeSort(arr, l, m)
 		mergeSort(arr, m+1, r)
 		merge(arr, l, m, r)
 
-
-# Driver code to test above
 arr = [12, 11, 13, 5, 6, 7]
 n = len(arr)
 print("Given array is")
@@ -76,5 +56,3 @@ mergeSort(arr, 0, n-1)
 print("\n\nSorted array is")
 for i in range(n):
 	print("%d" % arr[i],end=" ")
-
-# This code is contributed by Mohit Kumra
